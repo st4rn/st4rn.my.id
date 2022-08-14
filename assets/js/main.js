77b38051b0d25895572152e9379f129176f7170f
Jean Tirstan <48832536+st4rn@users.noreply.github.com>
2022-08-14 13:54:53 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Jean Tirstan.", "Security Researcher.", "Cyber Security.", "CTF Player."],
       typeSpeed: 100,
       loop: true,
     });
